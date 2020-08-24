386f42066fddac1ebf6b3c77bc443d88268726c1
Kevin Henriquez <khenriquez@acid.cl>
2020-08-24 12:39:52 -0400

    test(kh, One Signal): initial test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { Fragment } from 'react'
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async="" />
+        <script dangerouslySetInnerHTML={{__html: `window.OneSignal=window.OneSignal||[];OneSignal.push(function(){OneSignal.init({appId: "8375f0e1-7429-43b7-b05b-8637674a5b9b",});});`}}/>
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default MyApp
