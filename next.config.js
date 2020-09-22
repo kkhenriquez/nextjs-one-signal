d5d2687d1c089c6fc86c79bc270278708bce7a64
Kevin Henriquez <khenriquez@acid.cl>
2020-09-22 17:28:38 -0300

    corregir path para los headers

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/OneSignalSDKWorker.js',
+        source: '/static/OneSignalSDKWorker.js',
         headers: [
           {
             key: 'Service-Worker-Allowed',
@@ -11,7 +11,7 @@ module.exports = {
         ]
       },
       {
-        source: '/OneSignalSDKUpdaterWorker.js',
+        source: '/static/OneSignalSDKUpdaterWorker.js',
         headers: [
           {
             key: 'Service-Worker-Allowed',
